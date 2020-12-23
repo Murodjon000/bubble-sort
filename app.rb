f99907cf8c674792aa40f00d8e9b0d70c4f2e25d
Murodjon <murodjon.tursunpulatov@gmail.com>
2020-12-23 14:23:54 +0500

    main sorting function added to bubble sort

--- app.rb
+++ app.rb
@@ -1,3 +1,17 @@
-def sort(num)
-    
+
+def bubble_sort(arr)
+len=arr.length-1
+    len.times do |i|
+        for i in 0..len
+          unless arr.find_index(arr[i]) === len
+            if arr[i]>arr[i+1]
+              arr[i],arr[i+1]=arr[i+1],arr[i]
+            end
+          end
+        end
+     end
+print arr
 end
+    
+     
+bubble_sort( [4,5,4,8,6,11,24,12,13,25,13,12,1])
